4f5f9ec336a55ebe75ddb772ff2eaba793d2c2fd
anuraggupta9118 <109300300+anuraggupta9118@users.noreply.github.com>
2024-03-18 07:22:30 +0000

    conversionOfDataYypes

---
+++ js1basic/03_conversionOperation.js
@@ -0,0 +1,28 @@
+let score=null
+
+//console.log(typeof score)
+//console.log(typeof(score))
+
+let valueInNumber= Number(score)
+//console.log(typeof valueInNumber)
+//console.log(valueInNumber)
+
+//"33"=> 33
+// "33abc"=> NaN
+// true => 1; false=>0
+
+let isLoggedIn =0
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+
+// 1=> true; 0=> false
+// ""=> false
+// "Anurag"=> true
+
+let someNumber=22
+
+let stringNumber=String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber); 

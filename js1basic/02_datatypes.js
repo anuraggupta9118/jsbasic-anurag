888c93cabc7b93131259d88cf6d8258f253050c0
anuraggupta9118 <109300300+anuraggupta9118@users.noreply.github.com>
2024-03-18 06:52:45 +0000

    datatypes

---
+++ js1basic/02_datatypes.js
@@ -0,0 +1,28 @@
+"use strict";// treat all js code as newer version
+
+//alert("Hello")// we are using node js , not browser
+
+let name ="Anurag"
+let age= 23
+let isLoggedIn=true
+
+//number=>2 to power 53
+// bigint
+//String=>""
+//boolean=> true/false
+// null=> Standalone value
+// undefined=> decleared a variable but not  assigned a value to that variable
+// symbol => unique
+
+
+
+//object 
+
+console.log(typeof " Anurag")
+
+console.log(typeof 34)
+
+console.log(typeof undefined)// undefined
+
+console.log(typeof null)// object
+

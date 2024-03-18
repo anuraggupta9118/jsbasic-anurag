a269c85c2b7d17e42ffc9a1bf3805c5c7cbd8624
anuraggupta9118 <109300300+anuraggupta9118@users.noreply.github.com>
2024-03-18 09:00:34 +0000

    negativeconversion

---
+++ js1basic/03_negativeConversion.js
@@ -0,0 +1,34 @@
+let value=4
+let negValue=-value
+//console.log(negValue)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**2)
+// console.log(2/3)
+// console.log(2%3)
+
+
+let str1="Hello"
+let str2=" Anurag"
+
+let str3=str1+str2
+// console.log(str3)
+
+// console.log("1"+2)
+// console.log(1+"2")
+// console.log("1"+2+2)
+// console.log(1+2+"2")
+
+// console.log((3+4)*(5%3))
+
+
+let gameCounter=100
+++gameCounter;
+console.log(gameCounter)
+
+
+//link to study
+//http://tc39.es/ecma262.multipage/abstract-operations.html#sec-type-conversion
+
